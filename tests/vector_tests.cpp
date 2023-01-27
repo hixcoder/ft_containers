e8fa66557c5450d88fe40483b3c7abe223b3ca5c
Hamza Boumahdi <hboumahd@e1r2p12.1337.ma>
2023-01-27 14:10:58 +0100

    finish std::iterator_traits

--- tests/vector_tests.cpp
+++ tests/vector_tests.cpp
@@ -6,16 +6,17 @@
 /*   By: hboumahd <hboumahd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 11:29:28 by hboumahd          #+#    #+#             */
-/*   Updated: 2023/01/26 09:40:21 by hboumahd         ###   ########.fr       */
+/*   Updated: 2023/01/27 11:21:35 by hboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "tests.hpp"
+// #include "tests.hpp"
 #include <vector>
 
 void vectorTest()
 {
   // std::vector
   std::vector<int> v1 = {445, 78, 57, 89, 20};
+  std::vector<int>::iterator it;
   
 }
--- utiles/iterators.hpp
+++ utiles/iterators.hpp
@@ -6,34 +6,55 @@
 /*   By: hboumahd <hboumahd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 10:49:57 by hboumahd          #+#    #+#             */
-/*   Updated: 2023/01/25 11:24:53 by hboumahd         ###   ########.fr       */
+/*   Updated: 2023/01/27 12:48:09 by hboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 # include <string>
 # include <iostream>
+# include <cstddef>
 
 // std::iterator_traits
 namespace ft
 {
+    struct random_access_iterator_tag {};
+    
+    template <class Category, class T, class Distance = ptrdiff_t,
+          class Pointer = T*, class Reference = T&>
+    struct iterator 
+    {
+        typedef T         value_type;
+        typedef Distance  difference_type;
+        typedef Pointer   pointer;
+        typedef Reference reference;
+        typedef Category  iterator_category;
+    };
     template <class Iterator> 
     class iterator_traits
     {
-        
+        typedef typename Iterator::difference_type      difference_type;
+        typedef typename Iterator::value_type           value_type;
+        typedef typename Iterator::pointer              pointer;
+        typedef typename Iterator::reference            reference;
+        typedef typename Iterator::iterator_category    iterator_category;
     };
     template <class T> 
     class iterator_traits<T*>
     {
-        
+        typedef ptrdiff_t                   difference_type;
+        typedef T                           value_type;
+        typedef T*                          pointer;
+        typedef T&                          reference;
+        typedef random_access_iterator_tag  iterator_category;
     };
     template <class T> 
     class iterator_traits<const T*>
     {
-        typedef ptrdiff_t difference_type;
-        typedef T value_type;
-        typedef T* pointer;
-        typedef T& reference;
-        // typedef random_access_iterator_tag iterator_category;
+        typedef ptrdiff_t                   difference_type;
+        typedef T                           value_type;
+        typedef const T*                    pointer;
+        typedef const T&                    reference;
+        typedef random_access_iterator_tag  iterator_category;
     };
 }
