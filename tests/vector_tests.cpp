3b8afbc54412547da48ddaa7e3d38d832047fefd
hixcoder <hixcoder@gmail.com>
2023-02-17 16:20:21 +0100

    continue the tests

--- tests/vector_tests.cpp
+++ tests/vector_tests.cpp
@@ -6,7 +6,7 @@
 /*   By: hboumahd <hboumahd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 11:29:28 by hboumahd          #+#    #+#             */
-/*   Updated: 2023/02/15 13:31:57 by hboumahd         ###   ########.fr       */
+/*   Updated: 2023/02/17 15:41:36 by hboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -173,25 +173,165 @@ void test4_elementaccess(int is_std)
 {
   if (is_std)
   {
-    std::cout << "std: capacity\n";
+    std::cout << "std: Element access\n";
     std::cout << "-------------------\n";
-    std::vector<int> v(9, 100);
-      
+    std::vector<int> v;
+    v.push_back(45);
+    v.push_back(445);
+    v.push_back(345);
+    v.push_back(745);
+    v.push_back(6);
+    v.push_back(32);
+    // test operator[]
+    std::cout << "v[i]   : ";
+    for (size_t i = 0; i < v.size(); i++)
+      std::cout << v[i] << " ";
+    std::cout << "\n";
+
+    // test at() function
+    std::cout << "v.at(i): ";
+    for (size_t i = 0; i < v.size(); i++)
+      std::cout << v.at(i) << " ";
+    std::cout << "\n";
+    
+    // test other functions
+    std::cout << "v.front() = " << v.front() << "\n";
+    
+    std::cout << "v.back() = " << v.back() << "\n";
+    
+    int* p = v.data();
+    std::cout << "p[i]: ";
+    for (size_t i = 0; i < 3; i++)
+      std::cout << p[i] << " ";
+    std::cout << "\n";
   }
   else
   {
-   
-    std::cout << "\ntest3: ok\n";
+    std::cout << "\nft: Element access\n";
+    std::cout << "-------------------\n";
+    ft::vector<int> v;
+    v.push_back(45);
+    v.push_back(445);
+    v.push_back(345);
+    v.push_back(745);
+    v.push_back(6);
+    v.push_back(32);
+    // test operator[]
+    std::cout << "v[i]   : ";
+    for (size_t i = 0; i < v.size(); i++)
+      std::cout << v[i] << " ";
+    std::cout << "\n";
+
+    // test at() function
+    std::cout << "v.at(i): ";
+    for (size_t i = 0; i < v.size(); i++)
+      std::cout << v.at(i) << " ";
+    std::cout << "\n";
+    
+    // test other functions
+    std::cout << "v.front() = " << v.front() << "\n";
+    
+    std::cout << "v.back() = " << v.back() << "\n";
+    
+    int* p = v.data();
+    std::cout << "p[i]: ";
+    for (size_t i = 0; i < 3; i++)
+      std::cout << p[i] << " ";
+    std::cout << "\n";
+    
+    std::cout << "\ntest4: ok\n";
     std::cout << "-------------------\n";
 
   }
 }
 
+void test5_modifiers(int is_std)
+{
+  if (is_std)
+  {
+    std::cout << "std: Element access\n";
+    std::cout << "-------------------\n";
+
+    // test assign function -------------------------------------
+    std::vector<int> first;
+    std::vector<int> second;
+    std::vector<int> third;
+
+    first.assign (7,100);             // 7 ints with a value of 100
+
+    std::vector<int>::iterator it0;
+    it0=first.begin()+1;
+
+    second.assign (it0,first.end()-1); // the 5 central values of first
+
+    int myints[] = {1776,7,4};
+    third.assign (myints,myints+3);   // assigning from array.
+
+    std::cout << "Size of first: " << int (first.size()) << '\n';
+    std::cout << "Size of second: " << int (second.size()) << '\n';
+    std::cout << "Size of third: " << int (third.size()) << '\n';
+
+
+    // test push_back -------------------------------------
+    ft::vector<int> v;
+    v.push_back(45);
+    v.push_back(445);
+    v.push_back(34445);
+    v.push_back(4345);
+    v.push_back(5);
+    v.push_back(445);
+    v.push_back(3445);
+    std::cout << "v[i]   : ";
+    for (size_t i = 0; i < v.size(); i++)
+      std::cout << v[i] << " ";
+
+
+    // test pop_back -------------------------------------
+    for (size_t i = 0; i < 3; i++)
+      v.pop_back();
+    for (size_t i = 0; i < v.size(); i++)
+      std::cout << v[i] << " ";
+
+    // test insert -------------------------------------
+    std::vector<int> myvector (3,100);
+    std::vector<int>::iterator it;
+    
+    it = myvector.begin();
+    it = myvector.insert ( it , 200 );
+    
+    myvector.insert (it,2,300);
+    
+    // "it" no longer valid, get a new one:
+    it = myvector.begin();
+    
+    std::vector<int> anothervector (2,400);
+    myvector.insert (it+2,anothervector.begin(),anothervector.end());
+    
+    int myarray [] = { 501,502,503 };
+    myvector.insert (myvector.begin(), myarray, myarray+3);
+    
+    std::cout << "myvector contains:";
+    for (it=myvector.begin(); it<myvector.end(); it++)
+      std::cout << ' ' << *it;
+    std::cout << '\n';
+
+    
+    // test erase -------------------------------------
+    
+  }
+  else
+  {
+    std::cout << "\nft: Element access\n";
+
+
+  }
+}
 void vectorTest()
 {
   int is_std = 1;
   // test1_construction(is_std);
   // test2_iterators(is_std);
   // test3_capacity(is_std);
-  test4_elementaccess(is_std);
+  // test4_elementaccess(is_std);
+  test5_modifiers(is_std);
 }
